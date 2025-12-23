93783b7375a04255ab15a98df0f50def0bce693d
mikeynic789 <mikeynic28@gmail.com>
2025-12-23 17:42:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ function dogset() {
         select.innerHTML=`<select class=\"selector\"><option class=\"all\">all</option><option class=\"hershey\">Hershey</option><option class=\"cannoli\">Cannoli</option><option class=\"kuma\">Kuma</option></select>`;
         photos.innerHTML=`<img src=\"Cano1.jpeg\" class=\"cano\"><img src=\"Cano2.jpeg\" class=\"cano1\"><img src=\"Cano3.jpeg\"\
  class=\"cano\"><img src=\"Cano4.jpeg\" class=\"cano\"><img src=\"Cano5.jpeg\" class=\"cano\">
- <img src=\"Kuma1.jpeg\" class=\"kuma\">
- <img src=\"Kuma2.jpeg\" class=\"kuma\">
- <img src=\"Kuma3.jpeg\" class=\"kuma\">
- <img src=\"Kuma4.jpeg\" class=\"kuma1\">
- <img src=\"Kuma5.jpeg\" class=\"kuma1\">`}else{input.value=""}
+ <img src=\"Kuma1.jpeg\" class=\"Kuma\">
+ <img src=\"Kuma2.jpeg\" class=\"Kuma\">
+ <img src=\"Kuma3.jpeg\" class=\"Kuma\">
+ <img src=\"Kuma4.jpeg\" class=\"Kuma1\">
+ <img src=\"Kuma5.jpeg\" class=\"Kuma1\">`}else{input.value=""}
 };
 
 enter.addEventListener("click",() => dogset());
