652c3cb5e7bce1c28d55753028d63ff49fdf701a
mikeynic789 <mikeynic28@gmail.com>
2025-12-23 17:19:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const photos=document.querySelector(".photo-container")
 function dogset() {
     if (input.value=="holycannoli") {
         select.innerHTML=`<select class=\"selector\"><option class=\"all\">all</option>><option class=\"hershey\">Hershey</option><option class=\"cannoli\">Cannoli</option><option class=\"kuma\">Kuma</option>`;
-        photos.innerHTML=`<img src=\"cano1.jpeg\" class=\"cano\"><img src=\"cano2.jpeg\" class=\"cano1\"><img src=\"cano3.jpeg\"\
- class=\"cano\"><img src=\"cano4.jpeg\" class=\"cano\"><img src=\"cano5.jpeg\" class=\"cano\">
+        photos.innerHTML=`<img src=\"Cano1.jpeg\" class=\"cano\"><img src=\"Cano2.jpeg\" class=\"cano1\"><img src=\"Cano3.jpeg\"\
+ class=\"cano\"><img src=\"Cano4.jpeg\" class=\"cano\"><img src=\"Cano5.jpeg\" class=\"cano\">
  <img src=\"Kuma1.jpeg\" class=\"kuma\">
  <img src=\"Kuma2.jpeg\" class=\"kuma\">
  <img src=\"Kuma3.jpeg\" class=\"kuma\">
