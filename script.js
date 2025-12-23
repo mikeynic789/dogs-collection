4f9de2f30a7062ae90ce7c8ab849c5ddd05150f1
mikeynic789 <mikeynic28@gmail.com>
2025-12-23 16:02:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const select=document.querySelector(".select")
 const photos=document.querySelector(".photo-container")
 function dogset() {
     if (input.value=="holycannoli") {
-	select.innerHTML="<select class=\"selector\"><option class=\"all\">all</option>><option class=\"hershey\">Hershey</option>><option class=\"cannoli\">Cannoli</option>><option class=\"kuma\">Kuma</option>";
-	photos.innerHTML="<img src=\"cano1.jpeg\" class=\"cano\"><img src=\"cano2.jpeg\" class=\"cano\"><img src=\"cano3.jpeg\" class=\"cano\"><img src=\"cano4.jpeg\" class=\"cano\"><img src=\"cano5.jpeg\" class=\"cano\">"}else{input.value=""}
+        select.innerHTML="<select class=\"selector\"><option class=\"all\">all</option>><option class=\"hershey\">Hershey</optio\
+n>><option class=\"cannoli\">Cannoli</option>><option class=\"kuma\">Kuma</option>";
+        photos.innerHTML="<img src=\"cano1.jpeg\" class=\"cano\"><img src=\"cano2.jpeg\" class=\"cano1\"><img src=\"cano3.jpeg\"\
+ class=\"cano\"><img src=\"cano4.jpeg\" class=\"cano\"><img src=\"cano5.jpeg\" class=\"cano\">"}else{input.value=""}
 };
 
 enter.addEventListener("click",() => dogset());
