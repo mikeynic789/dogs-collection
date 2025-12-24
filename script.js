f53bb0e055a93da31b7aeca27993e514d2cccf98
mikeynic789 <mikeynic28@gmail.com>
2025-12-24 14:11:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const photos=document.querySelector(".photo-container")
 function dogset() {
     if (input.value=="holycannoli") {
         select.innerHTML=`<select class=\"selector\"><option class=\"all\">all</option><option class=\"hershey\">Hershey</option><option class=\"cannoli\">Cannoli</option><option class=\"kuma\">Kuma</option></select>`;
+        selected=document.querySelector(".selector");
         photos.innerHTML=`<img src=\"Cano1.jpeg\" class=\"cano\"><img src=\"Cano2.jpeg\" class=\"cano1\"><img src=\"Cano3.jpeg\"\
  class=\"cano\"><img src=\"Cano4.jpeg\" class=\"cano\"><img src=\"Cano5.jpeg\" class=\"cano\">
  <img src=\"Kuma1.jpeg\" class=\"Kuma\">
@@ -15,3 +16,16 @@ function dogset() {
 };
 
 enter.addEventListener("click",() => dogset());
+function which(n) {
+    if (n=="Cannoli") {
+        photos.innerHTML=`<img src=\"Cano1.jpeg\" class=\"cano\"><img src=\"Cano2.jpeg\" class=\"cano1\"><img src=\"Cano3.jpeg\"\
+ class=\"cano\"><img src=\"Cano4.jpeg\" class=\"cano\"><img src=\"Cano5.jpeg\" class=\"cano\">`
+    } else if(n=="Kuma") {
+        photos.innerHTML=`<img src=\"Kuma1.jpeg\" class=\"Kuma\">
+ <img src=\"Kuma2.jpeg\" class=\"Kuma\">
+ <img src=\"Kuma3.jpeg\" class=\"Kuma\">
+ <img src=\"Kuma4.jpeg\" class=\"Kuma1\">
+ <img src=\"Kuma5.jpeg\" class=\"Kuma1\">`
+    }
+
+selected.addEventListener("change",() => which(selected.value));
